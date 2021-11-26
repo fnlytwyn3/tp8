f6e15288427f61c3d42b05d327966821abcd9acf
Fallon <fallon.lytwyn@wsu.edu>
2021-11-26 21:23:15 +0000

    message

--- scripts.js
+++ scripts.js
@@ -1,76 +1,115 @@
 
-
 // function to asynchronously fetch file contents from path/URL "fromFile" 
 // and insert them in the DOM object found with "whereTo" -- note this
 // uses document.querySelector, so use CSS notation on "whereTo"
 
 function loadFileInto(fromFile, whereTo) {
 
-	// 1. creating a new XMLHttpRequest object
-	ajax = new XMLHttpRequest();
+  // 1. creating a new XMLHttpRequest object
+  ajax = new XMLHttpRequest();
 
-	// 2. defines the GET/POST method, the source, and the async value of the AJAX object
-	ajax.open("GET", fromFile, true);
+  // 2. defines the GET/POST method, the source, and the async value of the AJAX object
+  ajax.open("GET", fromFile, true);
 
-	// 3. provides code to do something in response to the AJAX request
-	ajax.onreadystatechange = function() {
+  // 3. provides code to do something in response to the AJAX request
+  ajax.onreadystatechange = function() {
 
-		if ((this.readyState == 4) && (this.status == 200)) { // if .readyState is 4, the process is done; and if .status is 200, there were no HTTP errors
+    if ((this.readyState == 4) && (this.status == 200)) { // if .readyState is 4, the process is done; and if .status is 200, there were no HTTP errors
 
-			document.querySelector(whereTo).innerHTML = this.responseText; // insert received output directly into the chosen DOM object
+      document.querySelector(whereTo).innerHTML = this.responseText; // insert received output directly into the chosen DOM object
 
-		} else if ((this.readyState == 4) && (this.status != 200)) { // if .readyState is 4, the process is done; and if .status is NOT 200, output the error into the console
+    } else if ((this.readyState == 4) && (this.status != 200)) { // if .readyState is 4, the process is done; and if .status is NOT 200, output the error into the console
 
-			console.log("Error: " + this.responseText);
+      console.log("Error: " + this.responseText);
 
-		}
+    }
 
-	} // end ajax.onreadystatechange function
+  } // end ajax.onreadystatechange function
 
-	// 4. let's go -- initiate request and process the responses
-	ajax.send();
+  // 4. let's go -- initiate request and process the responses
+  ajax.send();
 
 }
 
-window.onload = function()
-{
-  document.querySelector("#r1").onclick = function()
-  {
-BestChocolateChipCookies.displayRecipe();
-}
-  
-  loadFileInto("ingredients.html" , "#ingredients ul");
-  loadFileInto("equipment.html" , "#equipment ul");
-  loadFileInto("directions.html" , "#directions ol");
-}
-function myFunction() 
-      {
-     document.getElementById("H1").style.color = "#DEB887";
+// new Recipe object
+function Recipe(recipeName, contributorName, imageURL, ingredientsFilename, equipmentFilename, directionsFilename) {
 
-      
-      }
- 
-function Recipe(recipeName, contributorName, imageURL, ingredientsFilename, equipmentFilename, directionsFilename)
-{
   this.recipe = recipeName;
   this.contributor = contributorName;
   this.img = imageURL;
   this.ingredients = ingredientsFilename;
   this.equipment = equipmentFilename;
   this.directions = directionsFilename;
-  
-  this.displayRecipe = function()
-  {
-    document.querySelector("#Batter h1").innerHTML = this.recipe;
+
+  this.displayRecipe = function() {
+
+    document.querySelector("#cookieHeader").innerHTML = this.recipe;
     document.querySelector("#contributor").innerHTML = this.contributor;
-    document.queery
+    document.querySelector("#cookieHeader").style.backgroundImage = "url(" + this.img + ")";
     
-    LoadFileInto(this.ingredients, "#ingredients ul");
-    LoadFileInto(this.equipment, "#equipment ul");
-    LoadFileInto(this.directions, "#directions ol");
+    loadFileInto(this.ingredients, "#ingredients ul");
+    loadFileInto(this.equipment, "#equipment ul");
+    loadFileInto(this.directions, "#directions ol");
   }
-  
+
 }
 
-BestChocolateChipCookies = new Recipe("Best Chocolate Chip Cookies", "Fallon", "https://images.unsplash.com/photo-1499636136210-6f4ee915583e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=464&q=80", "ingredients.html", "equipment.html" , "directions.html");
+BestChocolateChipCookie = new Recipe("Best Chocolate Chip Cookies", 
+                        "Fallon Lytwyn", 
+                        "cookie.jpg", 
+                        "ingredients.html", 
+                        "equipment.html", 
+                        "directions.html");
 
+
+
+ SiggSnickerdoodles = new Recipe("Mrs. Sigg's Snickerdoodles", 
+                          "Theo McBurney", 
+                          "snick.jpg", 
+                          "ingredients2.html", 
+                          "equipment2.html", 
+                          "directions2.html");
+
+
+CrumbedChicken = new Recipe ("Lemon Bars", "Cody Osborn", 
+                             "lem.jpg",
+                            "ingredients3.html",
+                            "equipment3.html",
+                            "directions3.html");
+
+
+window.onload = function() {
+
+  document.querySelector("#cookieHeader").classList.add("tp6");
+
+  document.querySelector("#ingredients").onclick = function() {
+    document.querySelector("#ingredients ul").classList.toggle("showMe");
+  }
+
+  document.querySelector("#equipment").onclick = function() {
+    document.querySelector("#equipment ul").classList.toggle("showMe");
+  }
+
+  document.querySelector("#directions").onclick = function() {
+    document.querySelector("#directions ol").classList.toggle("showMe");
+  }
+
+  document.querySelector("#cookieHeader").onclick = function() {
+    this.classList.toggle("tp6");
+  }
+
+document.querySelector("#footer").innerHTML += "<p>This was not my own recipe.</p>";
+
+document.querySelector("#r1").onclick = function() {
+  BestChocolateChipCookie.displayRecipe();
+}
+
+document.querySelector("#r2").onclick = function() {
+  SiggSnickerdoodles.displayRecipe();
+}
+
+document.querySelector("#r3").onclick = function() {
+  CrumbedChicken.displayRecipe();
+  
+}
+} // end of window.onload
